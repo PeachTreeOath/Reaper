5e89ce258ebd4cdfc13b7ff7a177b7331c162f66
PeachTreeOath <peachtreeoath@gmail.com>
2016-01-15 21:01:42 -0700

    Basic animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,13 @@ using System.Collections;
 public class Player : BoardObject
 {
 
+	private Animator animator;
 
 	// Use this for initialization
 	void Start ()
 	{
 		boardType = BoardType.PLAYER;
+		animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -32,21 +34,26 @@ public class Player : BoardObject
 				destR = Mathf.Clamp (row - 1, 0, 6);
 				direction = 4;
 			}
-
+			animator.SetInteger ("Direction", direction);
 			bool shift_down = false; 
 			shift_down = Input.GetKey(KeyCode.RightShift) || Input.GetKey(KeyCode.LeftShift);
+
 			//Debug.Log (string.Format("shift_down equals", shift_down)); 
 			if (direction != 0) {
 				
 				if (shift_down) {
-					Move(destR, destC);
-					mgr.SetPlayerPosition(destR, destC, this); 	
-					mgr.VacatePlayerPosition(row, col);
-				} else if (mgr.PushBlock (direction, destR, destC)) {
 					Move (destR, destC);
-					mgr.SetPlayerPosition(destR, destC, this); 
-					mgr.VacatePlayerPosition(row, col); 
+					mgr.SetPlayerPosition (destR, destC, this); 	
+					mgr.VacatePlayerPosition (row, col);
+				} else if (mgr.PushBlock (direction, destR, destC)) {
+					animator.SetBool ("Pushing", true);
+					Move (destR, destC);
+					mgr.SetPlayerPosition (destR, destC, this); 
+					mgr.VacatePlayerPosition (row, col); 
+				} else if (animator.GetBool ("Pushing")) {
+					animator.SetBool ("Pushing", false);
 				}
+					
 			}
 		} 
 	}
