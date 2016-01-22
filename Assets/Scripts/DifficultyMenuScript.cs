3bb758613cda19e8aa0cab5f4870a1e14b8f90c5
Doggyfoofoo <hyyaya@gmail.com>
2016-01-22 00:10:47 -0800

    Difficulty menu created
    script and hierarchy done, just need to plug in sprites

---
+++ Assets/Scripts/DifficultyMenuScript.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DifficultyMenuScript : MonoBehaviour {
+
+	enum arrow{size, difficulty}; 
+	enum size{five, seven, nine};
+	enum difficulty{easy, medium, hard};
+	int arrowAt = 0;  
+	int difficultyAt; 
+	int sizeAt; 
+
+	// Use this for initialization
+	void Start () {
+	
+		GameObject.Find ("SizeText").GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("Images/Size"); 
+		GameObject.Find ("DifficultyText").GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("Images/Difficulty");
+		GameObject.Find ("StartText").GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("Images/PressStart");
+		GameObject.Find ("SizeArrow").GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("Images/SelectionArrow");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.DownArrow) && (arrowAt ==  (int) arrow.size)) {
+			arrowAt = (int) arrow.difficulty; 
+			GameObject.Find ("SizeArrow").GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("None");
+			GameObject.Find ("DifficultyArrow").GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("Images/SelectionArrow");
+		
+		} else if (Input.GetKeyDown (KeyCode.UpArrow) && ( arrowAt == (int) arrow.difficulty)) {
+			arrowAt = (int) arrow.size;
+			GameObject.Find ("SizeArrow").GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("Images/SelectionArrow");
+			GameObject.Find ("DifficultyArrow").GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("None");
+		
+		} else if (Input.GetKeyDown (KeyCode.LeftArrow)) { // decrease settings
+
+			if (arrowAt == (int) arrow.size){
+				sizeAt = (int) Mathf.Clamp (sizeAt - 1, 0, (float) size.nine);
+				switch (sizeAt){
+				case (int) size.five: 
+					GameObject.Find ("Size").GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("Images/Five"); 
+					break; 
+				case (int) size.seven:
+					GameObject.Find ("Size").GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("Images/Seven"); 
+					break; 
+				case (int) size.nine: 
+					GameObject.Find ("Size").GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("Images/Nine"); 
+					break;
+				}
+			}else if (arrowAt == (int) arrow.difficulty){
+
+				difficultyAt = (int) Mathf.Clamp(difficultyAt - 1, 0, (float) difficulty.hard);
+				switch (difficultyAt){
+
+				case (int) difficulty.easy:
+					GameObject.Find ("Difficulty").GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("Images/Easy"); 
+					break;
+				case (int) difficulty.medium:
+					GameObject.Find ("Difficulty").GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("Images/Medium"); 
+					break;
+				case (int) difficulty.hard:
+					GameObject.Find ("Difficulty").GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("Images/Hard"); 
+					break;
+				}
+			}
+
+		} else if (Input.GetKeyDown (KeyCode.RightArrow)) { // increase settings
+
+			if (arrowAt == (int) arrow.size){
+				sizeAt = (int) Mathf.Clamp (sizeAt + 1, 0, (float) size.nine);
+				switch (sizeAt){
+				case (int) size.five: 
+					GameObject.Find ("Size").GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("Images/Five"); 
+					break; 
+				case (int) size.seven:
+					GameObject.Find ("Size").GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("Images/Seven"); 
+					break; 
+				case (int) size.nine: 
+					GameObject.Find ("Size").GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("Images/Nine"); 
+					break;
+				}
+			}else if (arrowAt == (int) arrow.difficulty){
+
+				difficultyAt = (int) Mathf.Clamp(difficultyAt + 1, 0, (float) difficulty.hard);
+				switch (difficultyAt){
+
+				case (int) difficulty.easy:
+					GameObject.Find ("Difficulty").GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("Images/Easy"); 
+					break;
+				case (int) difficulty.medium:
+					GameObject.Find ("Difficulty").GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("Images/Medium"); 
+					break;
+				case (int) difficulty.hard:
+					GameObject.Find ("Difficulty").GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("Images/Hard"); 
+					break;
+				}
+			}
+
+		} else if (Input.GetKeyDown (KeyCode.Return)){
+
+			Application.LoadLevel(2);
+		}
+	}
+
+
+}
--- Assets/Scripts/StartMenuScript.cs
+++ Assets/Scripts/StartMenuScript.cs
@@ -6,10 +6,6 @@ public class StartMenuScript : MonoBehaviour {
 	int numPlayers = 0; 
 	GameObject q1, q2, q3, q4, startButton; 
 
-
-	private float selectDelay = 0.2f;
-	private float selectTime = 0;
-
 	// Use this for initialization
 
 	void Awake () {
@@ -37,38 +33,30 @@ public class StartMenuScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
-		if (Input.GetKey (KeyCode.Space)) {
-			selectTime += Time.deltaTime; 
-			if (selectTime > selectDelay) {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			numPlayers += 1;
+		
+			switch (numPlayers) {
 
-				numPlayers += 1;
-				selectTime = 0; 
-
-				switch (numPlayers) {
-
-				case 1:
-					q1.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("None");
-					q1.GetComponent<Animator> ().runtimeAnimatorController = Resources.Load <RuntimeAnimatorController> ("Images/Kiwi/KiwiDanceController");
-					break;
-				case 2:
-					q2.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("None");
-					q2.GetComponent<Animator> ().runtimeAnimatorController = Resources.Load <RuntimeAnimatorController> ("Images/Kiwi/KiwiDanceController");
-					break;
-				case 3:
-					q3.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("None");
-					q3.GetComponent<Animator> ().runtimeAnimatorController = Resources.Load <RuntimeAnimatorController> ("Images/Kiwi/KiwiDanceController");
-					break;
-				case 4:
-					q4.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("None");
-					q4.GetComponent<Animator> ().runtimeAnimatorController = Resources.Load <RuntimeAnimatorController> ("Images/Kiwi/KiwiDanceController");
-					break;
-				}
+			case 1:
+				q1.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("None");
+				q1.GetComponent<Animator> ().runtimeAnimatorController = Resources.Load <RuntimeAnimatorController> ("Images/Kiwi/KiwiDanceController");
+				break;
+			case 2:
+				q2.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("None");
+				q2.GetComponent<Animator> ().runtimeAnimatorController = Resources.Load <RuntimeAnimatorController> ("Images/Kiwi/KiwiDanceController");
+				break;
+			case 3:
+				q3.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("None");
+				q3.GetComponent<Animator> ().runtimeAnimatorController = Resources.Load <RuntimeAnimatorController> ("Images/Kiwi/KiwiDanceController");
+				break;
+			case 4:
+				q4.GetComponent<SpriteRenderer> ().sprite = Resources.Load<Sprite> ("None");
+				q4.GetComponent<Animator> ().runtimeAnimatorController = Resources.Load <RuntimeAnimatorController> ("Images/Kiwi/KiwiDanceController");
+				break;
 			}
-		} else if ((numPlayers > 0) && (Input.GetKey (KeyCode.Return))) {
+		} else if ((numPlayers > 0) && (Input.GetKeyDown (KeyCode.Return))) {
 			Application.LoadLevel (1);
-		}else{
-			selectTime = 0;
 		}
-
 	}
 }
